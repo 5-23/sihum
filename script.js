93de680355dc423c45aa08cb66df4ab1dd819cec
5-23 dev <86705803+5-23@users.noreply.github.com>
2023-10-13 15:04:24 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const gimalStart = "2023-10-18T10:15:00";
+
+function countdown() {
+	const a = new Date(gimalStart);
+	const b = new Date();
+	if (b >= a) {
+		document.getElementsByClassName("title")[0].innerHTML = "⭐️경 중간고사끝 축⭐️";
+	} else {
+		const total = (a - b) / 1000;
+		document.getElementById("seconds").innerHTML = `<b style="color: red">Rmx까지</b> ${getFloatFixed(total, 2)} 초 남음`;
+	}
+}
+
+function formatTime(time) {
+	return time < 10 ? "0" + time : time;
+}
+
+function getFloatFixed(value, fixed) {
+	return parseFloat(Math.round(value * 100) / 100).toFixed(fixed);
+}
+
+setInterval(countdown, 10);
